bbe783dbe2353c49c8153cbadd8352f17e093791
Anantharam R U <49026944+anantha1999@users.noreply.github.com>
2021-02-21 19:12:29 +0530

    Update startpostgres.sh

--- startpostgres.sh
+++ startpostgres.sh
@@ -1,4 +1,2 @@
-initdb -D /usr/local/var/postgres/microservice1
-initdb -D /usr/local/var/postgres/microservice2
 pg_ctl -D /usr/local/var/postgres/microservice1 -o "-p 5434" start
 pg_ctl -D /usr/local/var/postgres/microservice2 -o "-p 5433" start
